53eb476eeadfd8376667bef306e90cf228a59614
myrka1 <myrkag93@gmail.com>
2023-12-23 23:45:06 -0800

    soundex key class test setup

---
+++ src/test/java/org/example/SoundexKeyTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+public class SoundexKeyTest {
+
+    @org.junit.Test
+    public void keyMaker() {
+    }
+
+    @org.junit.Test
+    public void letterCheck() {
+    }
+}
