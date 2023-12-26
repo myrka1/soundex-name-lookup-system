6098a8f86e4679f97bc3d56cb6cd8abd2d2e5acc
myrka1 <myrkag93@gmail.com>
2023-12-25 21:46:17 -0800

    junit letterCheck

--- src/test/java/org/example/SoundexKeyTest.java
+++ src/test/java/org/example/SoundexKeyTest.java
@@ -23,12 +23,30 @@ public class SoundexKeyTest {
         String name4 = "Knuth";
         String testKey4 = soundexMaker.keyMaker(name4);
         assertEquals("K530",testKey4);
-
     }
 
     @org.junit.Test
     public void letterCheck() {
         SoundexKey soundexMaker = new SoundexKey();
-        
+
+        Character letter1 = 'h';
+        int test1 = soundexMaker.letterCheck(letter1);
+        assertEquals(7,test1);
+
+        Character letter2 = '&';
+        int test2 = soundexMaker.letterCheck(letter2);
+        assertEquals(7,test2);
+
+        Character letter3 = 'm';
+        int test3 = soundexMaker.letterCheck(letter3);
+        assertEquals(5,test3);
+
+        Character letter4 = 'f';
+        int test4 = soundexMaker.letterCheck(letter4);
+        assertEquals(1,test4);
+
+        Character letter5 = 'q';
+        int test5 = soundexMaker.letterCheck(letter5);
+        assertEquals(2,test5);
     }
 }
